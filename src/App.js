48bb0e4cd9e61ff7ec41fa2872e7abcf8bb85322
Wong-Flo <florian.wong@outlook.com>
2024-09-20 17:11:22 +0200

    add changes to app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TextInput from './textInput.js';
 
 // initial useState for when loading the page
 export default function App() {
-  const [topText, setTopText] = useState(`meme`);
-  const [bottomText, setBottomText] = useState(`omg`);
+  const [topText, setTopText] = useState(`memes`);
+  const [bottomText, setBottomText] = useState(`such wow`);
   const [template, setTemplate] = useState('doge');
   const [memeUrl, setMemeUrl] = useState('');
   // url edit when typing into the Input fields
